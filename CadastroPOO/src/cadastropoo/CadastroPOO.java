e46b2d813a7310730134c18a75916ab217e67d5a
Otavio <oaalves@outlook.com.br>
2023-09-05 20:10:10 -0300

    Visual

--- CadastroPOO/src/cadastropoo/CadastroPOO.java
+++ CadastroPOO/src/cadastropoo/CadastroPOO.java
@@ -17,10 +17,9 @@ public class CadastroPOO {
         System.out.println("2 - Adicionar nova Pessoa Fisica");
         System.out.println("3 - Adicionar nova Pessoa Juridica");
 
-        
         Scanner entrada = new Scanner(System.in);
-        
-       int opCao = entrada.nextInt();
+
+        int opCao = entrada.nextInt();
         entrada.nextLine();
 
         boolean continuar = true;
@@ -30,17 +29,15 @@ public class CadastroPOO {
         switch (opCao) {
             case 1:  
             {
-                
                 while (continuar){
                     System.out.println("ID: ");
-                    int id = entrada.nextInt();                    
+                    int id = entrada.nextInt();   
+                                     
                     entrada.nextLine();                    
                     System.out.println("Nome: ");
                     String nome = entrada.nextLine();
                     
                     //Metodos para passar informações
-
-                    
                     pessoa.adicionarDados(id,nome);
                     
                     //Exibir
@@ -52,46 +49,23 @@ public class CadastroPOO {
                     
                     if (sair.equals("1")){
                         continuar = false;
-                        break;
-                    }
-                     break;
+                    }else{
+                        System.out.println("Acionou aqui"); 
                     }
                 }
+            }
             case 2:                 
-            
                 while (continuar){
-                    
-                    
-                    
                     System.out.println("ID: ");
                     int id1 = entrada.nextInt();
                     
                     entrada.nextLine();
                     System.out.println("Nome: ");
                     String nome1 = entrada.nextLine();
-                    
-                    /*if(continuar = pessoaFisica.adicionarDado(id, nome) == false){
-                        break;
-                    };*/
-                    
-                    
-
-
-                    
-                    
-                    
                     System.out.println("CPF: ");
                     String cpf = entrada.nextLine();
-                    
-                    
-                    
                     System.out.println("Idade: ");
                     int idade = entrada.nextInt();
-                    
-                    
-                    
-                    
-
                     
                     pessoaFisica.setCpf(cpf);
                     pessoaFisica.setIdade(idade);
@@ -101,104 +75,43 @@ public class CadastroPOO {
                     pessoaFisica.exibirFisica();
                     
                     System.out.println("Deseja Sair ? (1 = Sair)");
+                    entrada.nextLine();  // Consumir a nova linha pendente
                     String sair = entrada.nextLine();
-                    
-                    if (sair.equals("1")){
+
+                    if (sair.equals("1")) {
                         continuar = false;
                         break;
                     }
-                    
                 }
-                               
-           
-        //olá
-        case 3:
-            while (continuar){
-                System.out.println("ID: ");
-                int id = entrada.nextInt();
+            case 3:
+                while (continuar){
+                    System.out.println("ID: ");
+                    int id = entrada.nextInt();
                     
-                entrada.nextLine();
-                System.out.println("Nome: ");
-                String nome = entrada.nextLine();
-                System.out.println("CNPJ: ");
-                String cnpj = entrada.nextLine();
+                    entrada.nextLine();
+                    System.out.println("Nome: ");
+                    String nome = entrada.nextLine();
+                    System.out.println("CNPJ: ");
+                    String cnpj = entrada.nextLine();
                 
 
-                pessoaJuridica.setCnpj(cnpj);
+                    pessoaJuridica.setCnpj(cnpj);
                 
-                pessoaJuridica.adicionarJuridico(pessoa.getId(), pessoa.getNome(), pessoaJuridica.getCnpj());
-                pessoa.adicionarDados(id, nome);
+                    pessoaJuridica.adicionarJuridico(pessoa.getId(), pessoa.getNome(), pessoaJuridica.getCnpj());
+                    pessoa.adicionarDados(id, nome);
                 
-                pessoaJuridica.exibirJuridico();
+                    pessoaJuridica.exibirJuridico();
                 
-                System.out.println("Deseja Sair ? (1 = Sair)");
-                String sair = entrada.nextLine();
-                    
-                if (sair.equals("1")){
-                    continuar = false;
-                    break;
+                    System.out.println("Deseja Sair ? (1 = Sair)");
+                    entrada.nextLine();  // Consumir a nova linha pendente
+                    String sair = entrada.nextLine();
+
+                    if (sair.equals("1")) {
+                        continuar = false;
+                    }
                 }
-                break;
-            }
-        
-        
-        
+            break;
         }
+        entrada.close();
     }
 }
-
-    
-
-        
-        
-        
-        
-        
-        
-        
-        
-    
-
-                
-      /*  Pessoa.PessoaFisica pessoa1 = pessoa.new PessoaFisica(2, "Joao", "123.456.789-00", 30);
-        
-        
-        Pessoa.PessoaJuridica pessoa2 = pessoa.new PessoaJuridica(3, "Camila", "429.631.528-50");
-        pessoa2.exibir();
-
-        
-        
-        
-    }
-}
-
-
-
-
-        System.out.println("-----------------------------------");
-        System.out.println("Sistema de Pessoas Cadastradas");
-        System.out.println("");
-        System.out.println("");    
-        
-     
-        Pessoa pessoa = new Pessoa();
-        pessoa.id = 1;
-        pessoa.nome = "Otavio";
-        pessoa.exibir();
-        
-        Pessoa.PessoaFisica fisica = pessoa.new PessoaFisica();
-        pessoa.id = 2;
-        pessoa.nome = "Joao";
-        fisica.cpf = "429.631.528.50";
-        fisica.idade = 31;
-        
-        fisica.exibir();
-             
-        Pessoa.PessoaJuridica juridica = pessoa.new PessoaJuridica();
-        pessoa.id = 3;
-        pessoa.nome = "VISIONDEVS";
-        juridica.cnpj = "543.173.333/0001-10";
-        
-        juridica.exibir();
-         
-    } */
--- CadastroPOO/src/cadastropoo/model/Pessoa.java
+++ CadastroPOO/src/cadastropoo/model/Pessoa.java
@@ -1,42 +1,34 @@
 
 package cadastropoo.model;
-
 import java.util.HashMap;
-
 
 
 public class Pessoa{
     private int id;
     private String nome;
     private HashMap<Integer, String> cadastro;
-    
-
   
+    
     public Pessoa(int id, String nome){
         this.id = id;
         this.nome = nome;
-        this.cadastro = new HashMap();
+        this.cadastro = new HashMap<>();
     }
       
         public int getId(){
         return id;
-    };    
+        };    
         private void setId(int key){
         this.id = key;
-    };
-
-        
+        };       
         public String getNome(){
         return this.nome;  
-    };
+        };
         private void setNome(String n){
         this.nome = n;            
-    };     
+        };     
 
-
-    
-        
-        
+     
    public void adicionarDados(int id, String nome){
         cadastro.put(id,nome);
         
@@ -46,103 +38,69 @@ public class Pessoa{
     public void exibir(int key) {
         
         System.out.println(cadastro);
-        
-        
-         
     }
-
 
 
    public class PessoaFisica extends Pessoa{
        private String cpf;
        private int idade;
        private HashMap<Integer, String> cadastroFisico;
+      
        
-       
-       
-       public PessoaFisica(int id, String nome, String cpf, int idade){
+        public PessoaFisica(int id, String nome, String cpf, int idade){
         super(id, nome);
         this.cpf = cpf;
         this.idade = idade;
-        cadastroFisico = new HashMap();
-
-       }
+        cadastroFisico = new HashMap<>();
+        }
        
         public String getCpf(){
         return cpf;
-    };    
+        };    
        
         public void setCpf(String key){
-        this.cpf = key;
-           
-    }
+        this.cpf = key;           
+        }
  
-        public int getIdade(){
+        public int getIdade(){           
         return idade;  
-    };
+        };
        
-        public void setIdade(int n){
-        this.idade = n;            
-    };     
+        public void setIdade(Integer n){
+        this.idade = n;         
+        }   
+       
   
-   public void adicionarDado(int id, String nome, String cpf, int idade){
-       /*if(!cadastro.containsKey(id) && cadastro.get(id) == null){
-           System.out.println("Ola");
-           return false;
-       }else{
-        cadastroFisico.put(id,dados);
-        System.out.println("PessoaFisica adicionada com sucesso!");
-           return true; 
-       }*/
-       if (!cadastroFisico.containsKey(id) && !cadastro.containsKey(id)){
+        public void adicionarDado(int id, String nome, String cpf, int idade){
+            if (!cadastroFisico.containsKey(id) && !cadastro.containsKey(id)){
            
-           
-           
-           cadastroFisico.put(id,"Nome: "+nome+" CPF: "+cpf+" Idade: "+idade);
-           cadastro.put(id, nome);
-           /*cadastro.put(id,nome);*/
-           
-           super.adicionarDados(id, nome);
-           super.setId(id);
-           super.setNome(nome);
-           System.out.println("PessoaFisica adicionada com sucesso!");
-       }else{
-           System.out.println("Erro. id já está atribuido.");
-       }        
-               
-       
-       
-       
-        
-        
-       /* return true;
-        */
-    }
-   
+                super.adicionarDados(id, nome);
+                super.setId(id);
+                super.setNome(nome);
+                cadastro.put(getId(), getNome());
+                cadastroFisico.put(getId()," Nome: "+getNome()+ " CPF: "+getCpf()+" Idade: "+getIdade());          
+                System.out.println("PessoaFisica adicionada com sucesso!");
+            }else{
+                System.out.println("Erro. id já está atribuido.");
+            }        
+        }
 
-
-       
        public void exibirFisica(){
-           
-           
-           
-           System.out.println(cadastroFisico);
+            System.out.println(cadastroFisico);
             System.out.println(cadastro);
-       }
-   
-       
-       
-   
-     
-  } 
-  public class PessoaJuridica extends Pessoa{
+            System.out.println("Lista de CPF");
+        }
+} 
+  
+
+    public class PessoaJuridica extends Pessoa{
        private String cnpj;
        private HashMap<Integer, String> cadastroJuridico;
        
         public PessoaJuridica(int id, String nome, String cnpj) {
             super(id, nome);
             this.cnpj = cnpj;
-            this.cadastroJuridico = new HashMap();
+            this.cadastroJuridico = new HashMap<>();
         }
         
         public String getCnpj(){
@@ -161,96 +119,5 @@ public class Pessoa{
             System.out.println(cadastro);
             System.out.println(cadastroJuridico);
         };
-        
-        
-        
-       
-
-        
-    }
-   
-   
-
-}
-
-    
-   
-  
-   
-   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-interface Exibivel{
-    void exibir();
-}
-public class Pessoa implements Serializable, Exibivel{
-    public int id;
-    public String nome;  
-
-    public Pessoa(){
-       
-        
-    }
-    public void exibir(){
-        System.out.println(id); 
-        System.out.println(nome);         
-       
-    }
-  
-
- Classe Pessoa Fisica com atributos de heranca
-public class PessoaFisica implements Serializable, Exibivel{
-        public String cpf;
-        public int idade;
-        
-    public PessoaFisica() {
-        Pessoa.this.id = id;
-        Pessoa.this.nome = nome;
-        this.cpf = cpf;
-        this.idade = idade;
-    }
-    
-    public void exibir(){
-        System.out.println("ID: "+id); 
-        System.out.println("Nome: "+nome);
-        System.out.println("CPF: "+cpf);
-        System.out.println("idade: "+idade);
     }
 }
-
-public class PessoaJuridica implements Serializable, Exibivel{
-        public String cnpj;
-    public PessoaJuridica() {
-        Pessoa.this.id = id;
-        Pessoa.this.nome = nome;
-        this.cnpj = cnpj;
-        
-    }
-    public void exibir(){
-        System.out.println("ID: "+ id);
-        System.out.println("Nome: "+ nome);
-        System.out.println("CNPJ: "+ cnpj);
-    }
-}
-
-}*/
-
-
